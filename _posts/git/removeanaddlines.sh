6c2342017e1d9777a31e79670ff0e2ff6313cb55
Yupu <dongyupu1988@gmail.com>
2022-06-22 17:07:56 +0800

    Update removeanaddlines.sh

--- _posts/git/removeanaddlines.sh
+++ _posts/git/removeanaddlines.sh
@@ -1,1 +1,2 @@
+# 统计删除和添加的代码行数
 git log --pretty=tformat: --numstat | awk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }'
